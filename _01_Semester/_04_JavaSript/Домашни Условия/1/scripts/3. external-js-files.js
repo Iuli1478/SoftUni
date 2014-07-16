02637687aee4e5dd1f6b2301776dfc7e9f901e9b
iuli1478 <iuli.iuli17@gmail.com>
2014-07-17 02:11:11 +0300

    homework 2

--- _01_Semester/_04_JavaSript/Домашни Условия/1/scripts/3. external-js-files.js
+++
@@ -1,9 +0,0 @@
-﻿function onLogoClicked() {
-	console.log(this);
-	alert("You clicked the SoftUni Logo!");
-}
-
-function onGenderChange(e) {
-	var select = document.getElementById("gender-select");
-	alert(select.options[select.selectedIndex].value)
-}
